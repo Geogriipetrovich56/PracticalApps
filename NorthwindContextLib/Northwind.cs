c1210b5d2d7803ce6094bbe6898bf885d4ffbef2
Geogriipetrovich56 <Geogriipetrovich56@gmail.com>
2022-01-19 19:51:12 +0200

    delete 2

--- NorthwindContextLib/Northwind.cs
+++ NorthwindContextLib/Northwind.cs
@@ -16,31 +16,13 @@ namespace Packt.Shared
         {
         }
 
-        public virtual DbSet<AlphabeticalListOfProduct> AlphabeticalListOfProducts { get; set; } = null!;
         public virtual DbSet<Category> Categories { get; set; } = null!;
-        public virtual DbSet<CategorySalesFor1997> CategorySalesFor1997s { get; set; } = null!;
-        public virtual DbSet<CurrentProductList> CurrentProductLists { get; set; } = null!;
         public virtual DbSet<Customer> Customers { get; set; } = null!;
-        public virtual DbSet<CustomerAndSuppliersByCity> CustomerAndSuppliersByCities { get; set; } = null!;
-        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; } = null!;
         public virtual DbSet<Employee> Employees { get; set; } = null!;
-        public virtual DbSet<Invoice> Invoices { get; set; } = null!;
         public virtual DbSet<Order> Orders { get; set; } = null!;
         public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
-        public virtual DbSet<OrderDetailsExtended> OrderDetailsExtendeds { get; set; } = null!;
-        public virtual DbSet<OrderSubtotal> OrderSubtotals { get; set; } = null!;
-        public virtual DbSet<OrdersQry> OrdersQries { get; set; } = null!;
         public virtual DbSet<Product> Products { get; set; } = null!;
-        public virtual DbSet<ProductSalesFor1997> ProductSalesFor1997s { get; set; } = null!;
-        public virtual DbSet<ProductsAboveAveragePrice> ProductsAboveAveragePrices { get; set; } = null!;
-        public virtual DbSet<ProductsByCategory> ProductsByCategories { get; set; } = null!;
-        public virtual DbSet<QuarterlyOrder> QuarterlyOrders { get; set; } = null!;
-        public virtual DbSet<Region> Regions { get; set; } = null!;
-        public virtual DbSet<SalesByCategory> SalesByCategories { get; set; } = null!;
-        public virtual DbSet<SalesTotalsByAmount> SalesTotalsByAmounts { get; set; } = null!;
         public virtual DbSet<Shipper> Shippers { get; set; } = null!;
-        public virtual DbSet<SummaryOfSalesByQuarter> SummaryOfSalesByQuarters { get; set; } = null!;
-        public virtual DbSet<SummaryOfSalesByYear> SummaryOfSalesByYears { get; set; } = null!;
         public virtual DbSet<Supplier> Suppliers { get; set; } = null!;
         public virtual DbSet<Territory> Territories { get; set; } = null!;
 
@@ -57,42 +39,6 @@ namespace Packt.Shared
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<AlphabeticalListOfProduct>(entity =>
-            {
-                entity.ToView("Alphabetical list of products");
-            });
-
-            modelBuilder.Entity<CategorySalesFor1997>(entity =>
-            {
-                entity.ToView("Category Sales for 1997");
-            });
-
-            modelBuilder.Entity<CurrentProductList>(entity =>
-            {
-                entity.ToView("Current Product List");
-
-                entity.Property(e => e.ProductId).ValueGeneratedOnAdd();
-            });
-                        
-            modelBuilder.Entity<CustomerAndSuppliersByCity>(entity =>
-            {
-                entity.ToView("Customer and Suppliers by City");
-            });
-
-            modelBuilder.Entity<CustomerDemographic>(entity =>
-            {
-                entity.HasKey(e => e.CustomerTypeId)
-                    .IsClustered(false);
-
-                entity.Property(e => e.CustomerTypeId).IsFixedLength();
-            });
-
-            modelBuilder.Entity<Invoice>(entity =>
-            {
-                entity.ToView("Invoices");
-
-                entity.Property(e => e.CustomerId).IsFixedLength();
-            });
 
             modelBuilder.Entity<OrderDetail>(entity =>
             {
@@ -112,65 +58,6 @@ namespace Packt.Shared
                     .HasForeignKey(d => d.ProductID)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_Order_Details_Products");
-            });
-
-            modelBuilder.Entity<OrderDetailsExtended>(entity =>
-            {
-                entity.ToView("Order Details Extended");
-            });
-
-            modelBuilder.Entity<OrderSubtotal>(entity =>
-            {
-                entity.ToView("Order Subtotals");
-            });
-
-            modelBuilder.Entity<OrdersQry>(entity =>
-            {
-                entity.ToView("Orders Qry");
-
-                entity.Property(e => e.CustomerId).IsFixedLength();
-            });
-
-            modelBuilder.Entity<ProductSalesFor1997>(entity =>
-            {
-                entity.ToView("Product Sales for 1997");
-            });
-
-            modelBuilder.Entity<ProductsAboveAveragePrice>(entity =>
-            {
-                entity.ToView("Products Above Average Price");
-            });
-
-            modelBuilder.Entity<ProductsByCategory>(entity =>
-            {
-                entity.ToView("Products by Category");
-            });
-
-            modelBuilder.Entity<QuarterlyOrder>(entity =>
-            {
-                entity.ToView("Quarterly Orders");
-
-                entity.Property(e => e.CustomerId).IsFixedLength();
-            });
-
-            modelBuilder.Entity<SalesByCategory>(entity =>
-            {
-                entity.ToView("Sales by Category");
-            });
-
-            modelBuilder.Entity<SalesTotalsByAmount>(entity =>
-            {
-                entity.ToView("Sales Totals by Amount");
-            });
-
-            modelBuilder.Entity<SummaryOfSalesByQuarter>(entity =>
-            {
-                entity.ToView("Summary of Sales by Quarter");
-            });
-
-            modelBuilder.Entity<SummaryOfSalesByYear>(entity =>
-            {
-                entity.ToView("Summary of Sales by Year");
             });
 
             modelBuilder.Entity<Territory>(entity =>
